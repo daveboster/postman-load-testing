10a027cb442dda675424f9bd8da54fafdab17efd
Dave Boster <dave@boster.dev>
2022-11-30 16:24:44 -0600

    Pester refactor (#5)
    * refactor pestor and add integration tests
    
    * fix and workaround 😬 unused variable warnings
    
    * fix my hack trying to avoid unused variable ⚠️

---
+++ pester-api-testing/Invoke-ApiSmokeTests.ps1
@@ -0,0 +1,30 @@
+# Example: ./pester-api-testing/Invoke-ApiSmokeTests.ps1 -IntegrationBaseUrl 'https://localhost:7053/'                                                                                                   
+# Example: ./pester-api-testing/Invoke-ApiSmokeTests.ps1 -IntegrationBaseUrl 'https://postman-load-test.azurewebsites.net/' -PassThru 
+
+param (
+    [Parameter(Mandatory=$False)]
+    [string] $IntegrationBaseUrl,
+
+    [Parameter(Mandatory=$False)]
+    [string] $Output = 'Detailed',
+
+    [Parameter(Mandatory=$False)]
+    [switch] $PassThru
+)
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+Import-Module Pester
+
+$container = New-PesterContainer -Path "$PSScriptRoot/*.Smoke.Tests.ps1" -Data @{ IntegrationBaseUrl = $IntegrationBaseUrl }
+$pesterConfiguration = New-PesterConfiguration -Hashtable @{
+    Run = @{ # Run configuration.
+        Container = $container
+        PassThru = $PassThru # Return result object after finishing the test run.
+    }
+    Output = @{
+        Verbosity = $Output
+    }
+}
+
+Invoke-Pester -Configuration $pesterConfiguration
--- pester-api-testing/TaskSample.Smoke.Tests.ps1
+++ pester-api-testing/TaskSample.Smoke.Tests.ps1
@@ -1,12 +1,35 @@
-Describe 'Creating Tasks' {
+param (
+    [Parameter(Mandatory)]
+    [string] $IntegrationBaseUrl
+)
 
-    It 'It should create a task given a title' {
+BeforeAll {
+    # Add '-Verbose' to the Module commands to see what it's doing
+    # For troubleshooting, try uncommenting to remove module to ensure proper clean-up
+    #Get-Module -Name "$moduleUnderTest.psm1" -All  | Remove-Module -Force -ErrorAction Ignore
 
+    # Import Module Under Test
+    Import-Module "$PSScriptRoot/Rest-Api.psm1" -Force
+
+    # Set helper base-url from parameter
+    Set-ApiBaseUrl -Url $IntegrationBaseUrl
+}
+
+Describe "Creating Tasks for '$IntegrationBaseUrl'" {
+    BeforeAll {
         $expectedTitle = "Test";
 
-        $createdTask = Add-ApiResource "tasks/create?title=$($expectedTitle)"
+        $response = Add-ApiResource "tasks/create?title=$($expectedTitle)"
+        Write-Verbose "Response: $response"
+    }
 
-        $createdTask.title | Should -Be $expectedTitle
+    It 'It should return a successful response' {
+        $response.StatusCode | Should -Be 200
+    }
+
+    It 'It should return a task with our title' {
+        $task = $response.Content | ConvertFrom-Json
+        $task.title | Should -Be $expectedTitle
     }
 
 }
