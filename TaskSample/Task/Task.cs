98d383fa12dc479e1cec09a6bf7867fc154dbd58
Dave Boster <david.boster@amnhealthcare.com>
2023-02-22 12:38:07 -0600

    persist task (red-green)

--- TaskSample/Task/Task.cs
+++ TaskSample/Task/Task.cs
@@ -1,11 +1,20 @@
 namespace TaskSample.Task;
+
 public class Task
 {
+    internal static List<Task> Storage { get; set; } = new List<Task>();
+
     public string Title { get; set; }
     public Guid Id { get; set; } = Guid.NewGuid();
 
     public Task(string title = "")
     {
         this.Title = title;
+        Task.Storage.Add(this);
+    }
+
+    internal static Task Get(Guid guid)
+    {
+        return Task.Storage.FirstOrDefault(t => t.Id == guid);
     }
 }
