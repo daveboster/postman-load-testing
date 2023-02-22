1b0eb6c2c903b69bdaf0287e3b1d066ead4a7c15
Dave Boster <david.boster@amnhealthcare.com>
2023-02-22 11:57:47 -0600

    Task Id (refactor) remove duplicate unit test

--- TaskSample/Task/Task.Tests.cs
+++ TaskSample/Task/Task.Tests.cs
@@ -3,14 +3,6 @@ namespace TaskSample.Task;
 public class Tests
 {
     [Test]
-    public void Create_Empty()
-    {
-        Task task = new Task();
-
-        Assert.IsTrue(task != null);
-    }
-
-    [Test]
     public void Create_WithTitle()
     {
         string expectedTitle = "My Task Title";
